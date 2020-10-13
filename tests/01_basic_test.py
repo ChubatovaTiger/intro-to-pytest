c4575b3fc8566f0b500c4eb88df1f57d326247cf
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-10-13 17:04:49 +0200

    Update 01_basic_test.py

--- tests/01_basic_test.py
+++ tests/01_basic_test.py
@@ -1,3 +1,4 @@
+import time
 from other_code.services import DATA_SET_A, DATA_SET_B, DATA_SET_C
 
 
@@ -7,4 +8,5 @@ def test_example():
     """
     print("\nRunning test_example...")
     #assert DATA_SET_A == DATA_SET_B
+    time.sleep(10)
     assert 1 == 2
