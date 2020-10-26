199ce7bfc473bfa90be7f5e8263062b6085a90dd
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-10-26 20:34:46 +0100

    Create chubatovaPytest.py

---
+++ tests/chubatovaPytest.py
@@ -0,0 +1,23 @@
+import pytest
+import time
+
+
+def test_example1():
+    print("\nRunning example1... before pause")
+    time.sleep(60)
+    print("\nRunning example1... after pause")
+    assert 1 == 1
+    
+def test_example2():
+    print("\nRunning example2... before pause")
+    time.sleep(60)
+    print("\nRunning example2... after pause")
+    assert 1 == 1
+    
+def test_example3():
+    print("\nRunning example3... before pause")
+    time.sleep(60)
+    print("\nRunning example3... after pause")
+    assert 1 == 1
+    
+    
