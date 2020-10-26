43d23ebc345e7ef4551dfeb7545585c234272246
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-10-26 19:11:26 +0100

    Update 01_basic_test.py

--- tests/01_basic_test.py
+++ tests/01_basic_test.py
@@ -6,7 +6,7 @@ def test_example():
     """
     But really, test cases should be callables containing assertions:
     """
-    print("\nRunning test_example...")
+    print("\nRunning basic test... before pause")
     #assert DATA_SET_A == DATA_SET_B
     #assert DATA_SET_A == DATA_SET_B
     #assert DATA_SET_A == DATA_SET_B
@@ -20,6 +20,8 @@ def test_example():
     #assert DATA_SET_A == DATA_SET_B
     #assert DATA_SET_A == DATA_SET_B
     #assert DATA_SET_A == DATA_SET_B
+    time.sleep(60)
+    print("\nRunning basic test... after pause")
     print gp.setproduct(sys.argv[1])
     time.sleep(10)
     assert 1 == 2
