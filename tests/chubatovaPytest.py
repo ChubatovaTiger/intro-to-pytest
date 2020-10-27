b92ad55dd8b9cd70cf878fa6af99fe443589e579
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-10-27 10:11:40 +0100

    Update chubatovaPytest.py

--- tests/chubatovaPytest.py
+++ tests/chubatovaPytest.py
@@ -1,25 +1,15 @@
 import pytest
 import time
 
+class TestClass:
+    def test_one(self):
+        print("\ntest_one")
+        assert 1 == 1
 
-def test_example1():
-    print("\nRunning example1... before pause")
-    time.sleep(20)
-    print("\nRunning example1... after pause")
-    assert 1 == 1
-    
-def test_example2():
-    print("\nRunning example2... before pause")
-    time.sleep(20)
-    print("\nRunning example2... after pause")
-    time.sleep(20)
-    print("\nRunning example2... after second pause")
-    assert 1 == 2
-    
-def test_example3():
-    print("\nRunning example3... before pause")
-    time.sleep(20)
-    print("\nRunning example3... after pause")
-    assert 1 == 1
-    
+    def test_two(self):
+        print("\ntest_two")
+        assert 1 == 2
+
+
+
     
