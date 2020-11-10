eec10582760d62beae0595c53fdb6ca93594d1eb
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-11-10 15:17:04 +0100

    Update testAttrErr.py

--- testAttrErr.py
+++ testAttrErr.py
@@ -1,8 +1,4 @@
 def test_example():
-    print("\nRunning basic test... before pause")
     #assert 1 == 1
-    print("\nRunning basic test... after pause")
-    time.sleep(1)
     assert 1 == 2
-    
   spodf.sdkjks
