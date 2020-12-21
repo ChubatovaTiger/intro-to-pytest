a1fe6c37528e413978879273e4fdceae1edb8cc8
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-12-21 14:45:22 +0100

    Update 01_basic_test.py

--- tests/01_basic_test.py
+++ tests/01_basic_test.py
@@ -3,10 +3,10 @@
 import time
 from other_code.services import DATA_SET_A, DATA_SET_B, DATA_SET_C
 
-
+@pytest.mark.repeat(3)
 def test_example():
     print("\nRunning basic test1... before pause")
-    time.sleep(25)
+    time.sleep(10)
     print("\nRunning basic test1... after pause")
     assert 1 == 2
     
