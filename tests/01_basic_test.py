b197552dbf9b04302ad977aa1463a6a63f215e47
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-12-21 14:30:32 +0100

    Update 01_basic_test.py

--- tests/01_basic_test.py
+++ tests/01_basic_test.py
@@ -5,23 +5,13 @@ from other_code.services import DATA_SET_A, DATA_SET_B, DATA_SET_C
 
 
 def test_example():
-    """
-    But really, test cases should be callables containing assertions:
-    """
-    print("\nRunning basic test... before pause")
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
-    #assert DATA_SET_A == DATA_SET_B
+    print("\nRunning basic test1... before pause")
     time.sleep(25)
-    print("\nRunning basic test... after pause")
+    print("\nRunning basic test1... after pause")
+    assert 1 == 2
+    
+def test_example():
+    print("\nRunning basic test2... before pause")
+    time.sleep(25)
+    print("\nRunning basic test2... after pause")
     assert 1 == 2
