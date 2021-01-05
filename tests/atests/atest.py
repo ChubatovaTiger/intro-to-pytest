281435c3ab175e2ea6da52f4f8e3340f3d2c893b
AChubatova <64786092+AChubatova@users.noreply.github.com>
2021-01-05 16:47:04 +0100

    Create atest.py

---
+++ tests/atests/atest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import time
+from other_code.services import DATA_SET_A, DATA_SET_B, DATA_SET_C
+import pytest
+
+
+def atest_example():
+    print("\nRunning basic test1... before pause")
+    time.sleep(10)
+    print("\nRunning basic test1... after pause")
+    assert 1 == 2
